780ce647ec14437f2b6e16889a569eb450f5705e
mohanreddy626 <mohanreddie626@gmail.com>
2024-10-16 13:11:30 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import DataGridDemo from './DataGridDemo'
+
+export default function App() {
+  return (
+    <div>
+      <h1>DataGridDemo:-</h1>
+      <DataGridDemo />
+    </div>
+  )
+}
